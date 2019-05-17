50230c8aa32a85030b7b4b0615720b342058605f
shotakeu <taro20090819@yahoo.co.jp>
2019-05-17 14:27:33 +0900

    Masked URL for example.
    - Masked URL for example.

--- wpPostContentReplaceBookmarklet.js
+++ wpPostContentReplaceBookmarklet.js
@@ -2,14 +2,12 @@
 * bookmarklet
 *
 * compressor:http://refresh-sf.com/
-* javascript:!function(){var e="https://wandering-engineer.tech",n=document.getElementById("content"),t=n.value,l=t.replace(/http\:\/\/takelab\.sub\.jp/g,e);console.log("(σ・∀・)σｹﾞｯﾂ!!"),n.value=l,console.log("またつまらぬものを置換してしまったか・・・")}();
+* javascript:!function(){var e="https://wandering-engineer.tech",n=document.getElementById("content"),t=n.value,l=t.replace(/http\:\/\/*******\.***\.jp/g,e);console.log("(σ・∀・)σｹﾞｯﾂ!!"),n.value=l,console.log("またつまらぬものを置換してしまったか・・・")}();
 */
 (function(){
-  var replaceString = "https://wandering-engineer.tech";
+  var replaceString = "https://wandering-engineer.tech";// ブログの新URL
   var wpContent = document.getElementById('content');// 記事エリアオブジェクト
   var contentStr = wpContent.value;// 記事エリア文字列取得
-  var replacedContent = contentStr.replace(/http\:\/\/takelab\.sub\.jp/g, replaceString);
-  console.log("(σ・∀・)σｹﾞｯﾂ!!");
+  var replacedContent = contentStr.replace(/http\:\/\/*******\.***\.jp/g, replaceString); //旧URLを取得して置換
   wpContent.value = replacedContent;
-  console.log("またつまらぬものを置換してしまったか・・・");
 })();
